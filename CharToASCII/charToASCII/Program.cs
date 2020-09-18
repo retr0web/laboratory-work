fe481ced215e756ce2061dd21c0a1c0fbf404417
retr0web <45012672+retr0web@users.noreply.github.com>
2020-09-18 08:01:54 +0300

    Add files via upload

---
+++ CharToASCII/charToASCII/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace charToASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding ascii = Encoding.ASCII;
+            Console.WriteLine("Enter sequence of characters:");
+            Byte[] convertedAscii = ascii.GetBytes(Console.ReadLine());
+            foreach (Byte character in convertedAscii)
+                Console.Write(character < 100 ? "[{0,1:D3}]" : "[{0}]", character);
+        }
+    }
+}
